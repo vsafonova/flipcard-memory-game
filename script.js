c8e9a18e2f4924ece50857a294350d6417cf0063
vsafonova <viktoriyablak@gmail.com>
2023-12-04 16:56:56 +0100

    Writing a logic to checking cards

--- script.js
+++ script.js
@@ -1,45 +1,72 @@
 let gameContainerEl = document.querySelector("#gameContainer");
-
+let correctGuesses = 0;
 let cardsArray = [
-  "darkness.jpg",
-  "double.jpg",
-  "fairy.jpg",
-  "fighting.jpg",
-  "fire.jpg",
-  "grass.jpg",
-  "lightning.jpg",
-  "metal.jpg",
-  "psychic.jpg",
-  "water.jpg",
-  "darkness.jpg",
-  "double.jpg",
-  "fairy.jpg",
-  "fighting.jpg",
-  "fire.jpg",
-  "grass.jpg",
-  "lightning.jpg",
-  "metal.jpg",
-  "psychic.jpg",
-  "water.jpg",
+  {
+    name: "Darkness",
+    url: "darkness.jpg",
+  },
+  {
+    name: "double",
+    url: "double.jpg",
+  },
+  {
+    name: "fairy",
+    url: "fairy.jpg",
+  },
+  {
+    name: "fighting",
+    url: "fighting.jpg",
+  },
+  {
+    name: "fire",
+    url: "fire.jpg",
+  },
+  {
+    name: "grass",
+    url: "grass.jpg",
+  },
+  {
+    name: "lightning",
+    url: "lightning.jpg",
+  },
+  {
+    name: "metal",
+    url: "metal.jpg",
+  },
+  {
+    name: "psychic",
+    url: "psychic.jpg",
+  },
+  {
+    name: "water",
+    url: "water.jpg",
+  },
 ];
+cardsArray = cardsArray.concat(cardsArray);
+let neededGuesses = cardsArray.length / 2;
 
+//Card generator Function
 function createCards() {
   //Generate the HTML
   for (let i = 0; i < cardsArray.length; i++) {
     let card = document.createElement("div");
     let face = document.createElement("img");
     let back = document.createElement("div");
-
     card.classList = "card";
     face.classList = "face";
     back.classList = "back";
-    console.log(cardsArray[i]);
-    console.log("images/" + cardsArray[i]);
-    face.src = "images/" + cardsArray[i];
-   //Attch the cards to the gameContainer
+    //Attach the info to the cards
+    face.src = "images/" + cardsArray[i].url;
+    card.setAttribute("name", cardsArray[i].name);
+    //Attch the cards to the gameContainer
     gameContainerEl.appendChild(card);
     card.appendChild(face);
     card.appendChild(back);
+
+    card.addEventListener("click", function (e) {
+      card.classList.toggle("toggleCard");
+      compareCards(e);
+    });
   }
 }
 
@@ -48,7 +75,46 @@ function shuffleCards() {
   console.log(cardsArray);
 }
 
+//Check cards
+
+function compareCards(e) {
+  let clickedCard = e.target;
+  clickedCard.classList.add("flipped");
+
+  let flippedCards = document.querySelectorAll(".flipped");
+
+  //Logic
+  if (flippedCards.length === 2) {
+    if (
+      flippedCards[0].getAttribute("name") ===
+      flippedCards[1].getAttribute("name")
+    ) {
+      console.log("match");
+      winCondition();
+      flippedCards[0].style.pointerEvents = "none";
+      flippedCards[1].style.pointerEvents = "none";
+    } else {
+      console.log("wrong");
+      setTimeout(function () {
+        flippedCards[0].classList.remove("toggleCard");
+        flippedCards[1].classList.remove("toggleCard");
+      }, 1000);
+    }
+    flippedCards[0].classList.remove("flipped");
+    flippedCards[1].classList.remove("flipped");
+  }
+}
+
+function winCondition() {
+  correctGuesses += 1;
+  if (correctGuesses >= neededGuesses) {
+    console.log("You win");
+  }
+  
+  console.log(correctGuesses);
+  console.log(neededGuesses);
+}
+
 shuffleCards();
 
 createCards();
-
